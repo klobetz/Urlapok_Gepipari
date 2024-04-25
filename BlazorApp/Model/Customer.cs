6ab601ad7617b9a048577e7738102936ee0f33cd
Ákos Klobetz <klobetz@gmail.com>
2024-04-25 19:55:06 +0200

    Add project files.

---
+++ BlazorApp/Model/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int Price { get; set; }
+    }
+}
