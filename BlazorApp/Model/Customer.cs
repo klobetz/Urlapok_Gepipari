8e824c8048a446ea1c4292af884ca7c367d6d815
Ákos Klobetz <klobetz@gmail.com>
2024-04-29 17:54:50 +0200

    adatok megjelenítése táblázatban és listában

--- BlazorApp/Model/Customer.cs
+++ BlazorApp/Model/Customer.cs
@@ -1,13 +1,17 @@
-﻿namespace BlazorApp.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Model
 {
     public class Customer
     {
         public int Id { get; set; }
+
+       
         public string? Fname { get; set; }
         public string? Lname { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public DateTime BirthDay { get; set; }
+        public DateTime BirthDay { get; set; } = new DateTime(2024,01,01); //DateTime.Now;
         public int Price { get; set; }
     }
 }
